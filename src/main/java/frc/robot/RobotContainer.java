914235a803eefec61fa6de2ae3a430a884ea9960
Ryan Sjostrand <ryansjostrand@gmail.com>
2023-02-16 22:24:07 -0600

    Fancy auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -88,7 +88,12 @@ public class RobotContainer {
 
     // Initialize other autos here
     m_autoSelector.registerCommand("WPILibSwerveExample", "WPIS", makeWPILibSwerveExample());
-    // m_autoSelector.registerCommand("PathPlannerExample", "PPLE", makePathPlannerExample());
+    m_autoSelector.registerCommand("PathPlannerExample", "PPLE", makePathPlannerExample());
+
+    m_autoSelector.registerCommand("Auto Crap - Community", "CRAP", new SequentialCommandGroup(
+      new RunCommand(() -> m_drivetrain.drive(1, 0, 0, false), m_drivetrain).withTimeout(1.5),
+      new InstantCommand(() -> m_drivetrain.stop())
+    ));
 
     m_autoSelector.initialize();
   }
@@ -205,7 +210,7 @@ public class RobotContainer {
   public Command makePathPlannerExample() {
     // This will load the file "FullAuto.path" and generate it with a max velocity of 4 m/s and a max acceleration of 3 m/s^2
     // for every path in the group
-    List<PathPlannerTrajectory> pathGroup = PathPlanner.loadPathGroup("FullAuto", new PathConstraints(4, 3));
+    List<PathPlannerTrajectory> pathGroup = PathPlanner.loadPathGroup("FullAuto", new PathConstraints(1, 1));
 
     // This is just an example event map. It would be better to have a constant, global event map
     // in your code that will be used by all path following commands.
@@ -219,7 +224,7 @@ public class RobotContainer {
       m_drivetrain::resetOdometry, // Pose2d consumer, used to reset odometry at the beginning of auto
       Constants.DriveConstants.kDriveKinematics, // SwerveDriveKinematics
       new PIDConstants(5.0, 0.0, 0.0), // PID constants to correct for translation error (used to create the X and Y PID controllers)
-      new PIDConstants(0.5, 0.0, 0.0), // PID constants to correct for rotation error (used to create the rotation controller)
+      new PIDConstants(1.0, 0.0, 0.0), // PID constants to correct for rotation error (used to create the rotation controller)
       m_drivetrain::setModuleStates, // Module states consumer used to output to the drive subsystem
       eventMap,
       true, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
